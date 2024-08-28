a1deb3a352b2cba0de662e7dad651e7cd6daabf5
2Kronos <rayachrinovic@gmail.com>
2024-08-28 15:53:28 +0200

    New file

---
+++ Test of Matplotlib.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# Create some data
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 25, 30, 40]
+
+# Create a plot
+plt.plot(x, y)
+
+# Add a title
+plt.title('Simple Plot')
+
+# Show the plot
+plt.show()
