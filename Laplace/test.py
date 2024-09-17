287501d05e79b330fab87e1e985de2376ebf0c9b
2Kronos <rayachrinovic@gmail.com>
2024-09-17 15:30:47 +0200

    modify

--- Laplace/test.py
+++ Laplace/test.py
@@ -1,1 +1,17 @@
-print("This is file 1 ")
+# print("This is file 1 ")
+# print("This is the second attempt")
+
+import matplotlib.pyplot as plt
+x = [1,2,3]
+y = [5,7,4]
+
+x2 =[1,2,3]
+y2 = [10,14,12]
+
+plt.plot(x,y, label = 'First line')
+plt.plot(x2, y2, label = 'second line')
+plt.xlabel('Plot number')
+plt.ylabel('import var')
+plt.title('import Graph')
+plt.legend()
+plt.show()
